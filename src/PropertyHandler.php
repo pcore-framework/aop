f6c04a426a1ce8db5596e3f58852ef3b5dc5ae4a
Magomedcoder <info@magomedcoder.ru>
2022-06-09 23:10:57 +0300

    Added property and property visitor handler

---
+++ src/PropertyHandler.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PCore\Aop;
+
+use PCore\Aop\Collectors\PropertyAttributeCollector;
+
+/**
+ * Trait PropertyHandler
+ * @package PCore\Aop
+ * @github https://github.com/pcore-framework/aop
+ */
+trait PropertyHandler
+{
+
+    protected bool $__propertyHandled = false;
+
+    protected function __handleProperties(): void
+    {
+        if (!$this->__propertyHandled) {
+            foreach (PropertyAttributeCollector::getClassPropertyAttributes(self::class) as $property => $attributes) {
+                foreach ($attributes as $attribute) {
+                    $attribute->handle($this, $property);
+                }
+            }
+            $this->__propertyHandled = true;
+        }
+    }
+
+}
---
+++ src/PropertyHandlerVisitor.php
@@ -0,0 +1,110 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PCore\Aop;
+
+use PCore\Di\Reflection;
+use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\{ClassConstFetch, ConstFetch, FuncCall, MethodCall, StaticCall, Variable};
+use PhpParser\Node\Name;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\{Class_, ClassMethod, Expression, If_, TraitUse};
+use PhpParser\{NodeVisitorAbstract, ParserFactory};
+use ReflectionException;
+use ReflectionNamedType;
+use ReflectionUnionType;
+
+/**
+ * Class PropertyHandlerVisitor
+ * @package PCore\Aop
+ * @github https://github.com/pcore-framework/aop
+ */
+class PropertyHandlerVisitor extends NodeVisitorAbstract
+{
+
+    public function __construct(protected Metadata $metadata)
+    {
+    }
+
+    public function enterNode(Node $node)
+    {
+        if ($node instanceof ClassMethod && $node->name->toString() === '__construct') {
+            $this->metadata->hasConstructor = true;
+        }
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function leaveNode(Node $node)
+    {
+        if ($node instanceof Class_) {
+            $params = [];
+            $reflectionClass = Reflection::class($this->metadata->className);
+            if ($reflectionConstructor = $reflectionClass->getConstructor()) {
+                $declaringClass = $reflectionConstructor->getDeclaringClass()->getName();
+                if ($classPath = $this->metadata->loader->findFile($declaringClass)) {
+                    $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+                    $ast = $parser->parse(file_get_contents($classPath));
+                    foreach ($ast as $stmt) {
+                        if ($stmt instanceof Node\Stmt\Namespace_) {
+                            foreach ($stmt->stmts as $subStmt) {
+                                if ($subStmt instanceof Class_) {
+                                    foreach ($subStmt->stmts as $internal) {
+                                        if ($internal instanceof ClassMethod && $internal->name->toString() === '__construct') {
+                                            $params = $internal->getParams();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                foreach ($reflectionConstructor->getParameters() as $key => $reflectionParameter) {
+                    $type = $reflectionParameter->getType();
+                    if (is_null($type)
+                        || ($type instanceof ReflectionNamedType && $type->isBuiltin())
+                        || $type instanceof ReflectionUnionType
+                        || ($type->getName()) === 'Closure') {
+                        continue;
+                    } else {
+                        $params[$key]->type = new Name('\\' . $type->getName());
+                    }
+                }
+            }
+            $c = [];
+            if (!$this->metadata->hasConstructor) {
+                $constructor = new ClassMethod('__construct', [
+                    'params' => $params
+                ]);
+                $constructor->flags = 1;
+                if ($node->extends) {
+                    $constructor->stmts[] = new If_(new FuncCall(new Name('method_exists'), [
+                        new ClassConstFetch(new Name('parent'), 'class'),
+                        new String_('__construct')
+                    ]), [
+                        'stmts' => [
+                            new Expression(new StaticCall(
+                                new ConstFetch(new Name('parent')), '__construct',
+                                [new Arg(new FuncCall(new Name('func_get_args')), unpack: true),]
+                            ))
+                        ]
+                    ]);
+                }
+                $constructor->stmts[] = new Expression(new MethodCall(
+                    new Variable(new Name('this')), '__handleProperties'
+                ));
+                $c = [$constructor];
+            }
+            $node->stmts = array_merge([new TraitUse([new Name('\PCore\Aop\PropertyHandler'),])], $c, $node->stmts);
+        }
+        if ($node instanceof ClassMethod && $node->name->toString() === '__construct') {
+            array_unshift($node->stmts,
+                new Expression(new MethodCall(new Variable(new Name('this')), '__handleProperties'))
+            );
+        }
+    }
+
+}
