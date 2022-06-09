1a44af18c57c32a6abf985b2a4bab2bbcd13cd38
Magomedcoder <info@magomedcoder.ru>
2022-06-09 23:13:43 +0300

    Added exception handle process and property

---
+++ src/Exceptions/ProcessException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PCore\Aop\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Class ProcessException
+ * @package PCore\Aop\Exceptions
+ * @github https://github.com/pcore-framework/aop
+ */
+class ProcessException extends RuntimeException
+{
+}
---
+++ src/Exceptions/PropertyHandleException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PCore\Aop\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Class PropertyHandleException
+ * @package PCore\Aop\Exceptions
+ * @github https://github.com/pcore-framework/aop
+ */
+class PropertyHandleException extends RuntimeException
+{
+}
